a2f2c18e6cc34110c47f98ccf2065299aa829217
jcamato-work <amatojonathan.work@gmail.com>
2021-12-29 13:08:49 -0800

    Remove README

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://emanate-biostats-inc.github.io',
+  baseUrl: '/user-guide/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'emanate-biostats-inc', // Usually your GitHub org/user name.
+  projectName: 'user-guide', // Usually your repo name.
+  // trailingSlash: false,
 
   presets: [
     [
